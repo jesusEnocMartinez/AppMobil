c763ee01a5fb98b2240d64cb1cbcc6393d161fe4
J Enoc <54125340+jesusEnocMartinez@users.noreply.github.com>
2024-03-09 23:12:48 -0600

    Initial commit

---
+++ src/main/kotlin/com/globalfit/AppMobil/AppMobilApplication.kt
@@ -0,0 +1,11 @@
+package com.globalfit.AppMobil
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AppMobilApplication
+
+fun main(args: Array<String>) {
+	runApplication<AppMobilApplication>(*args)
+}
---
+++ src/test/kotlin/com/globalfit/AppMobil/AppMobilApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.globalfit.AppMobil
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AppMobilApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
